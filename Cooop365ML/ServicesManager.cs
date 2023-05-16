62bd9ad8a7c4c26b644e0944d9c1dd8af14f10f6
Kasper Holm <kasper.marhauer@gmail.com>
2023-05-16 10:54:23 +0200

    servies manager

---
+++ Cooop365ML/ServicesManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cooop365ML
+{
+    public static class ServicesManager
+    {
+        public static MauiAppBuilder UseCustomServices(this MauiAppBuilder builder)
+        {
+
+
+            return builder;
+        }
+
+        public static MauiAppBuilder UseCustomViews(this MauiAppBuilder builder)
+        {
+
+
+            return builder;
+        }
+
+        public static MauiAppBuilder UseCustomViewModels(this MauiAppBuilder builder)
+        {
+
+
+            return builder;
+        }
+    }
+}
